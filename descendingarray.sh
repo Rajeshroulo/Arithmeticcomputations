a265f05d158396632c2d9a73a518270495cddca7
Rajeshroulo <rajraval017@gmail.com>
2020-04-12 18:53:40 +0530

    array sorted in descending

--- descendingarray.sh
+++ descendingarray.sh
@@ -1,24 +1,19 @@
 #!/bin/bash -x
-
 echo Enter three values
 read a
 read b
 read c
-
 p=$(( $a+($b*$c) ))
 q=$(( ($a*$b)+$c ))
 r=$(( ($a/$b)+$c ))
 s=$(( ($a%$b)+$c ))
-
 declare -A r
 i=0
 r[((i++))]=$p
 r[((i++))]=$q
 r[((i++))]=$r
 r[((i++))]=$s
-
 echo ${r[@]}
-
 for(( i=0; i<3; i++ ))
 do
   for(( j=0; j<4; j++ ))
@@ -28,13 +23,9 @@ do
       temp=${r[i]}
       r[$i]=${r[j]}
       r[$j]=$temp
-
     fi
-
    done
-
 done
-
 for(( i=0; i<4; i++ ))
 do
   echo ${r[i]}
